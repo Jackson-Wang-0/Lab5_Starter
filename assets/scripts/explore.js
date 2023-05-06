c6a80a613a1fffca03a789ca9b20c32e21bf661d
Jackson-Wang-0 <zhw049@ucsd.edu>
2023-05-05 17:12:32 -0700

    Load voices for browser and update options, speak feature and img change

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,48 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // load voices
+  let synth = window.speechSynthesis;
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    let voiceSelect = document.getElementById("voice-select");
+    voiceSelect.innerHTML = "";
+
+    // add options for each voice
+    voices.forEach((voice, i) => {
+      let option = document.createElement("option");
+      option.value = i;
+      option.textContent = voice.name + " (" + voice.lang + ")";
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoiceList();
+  // update browser supported voices
+  if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = populateVoiceList;
+  }
+
+  // button clicked -> speak && change image
+  let speakButton = document.querySelector("button");
+  speakButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    let text = document.getElementById("text-to-speak").value;
+    let selectedVoice = document.getElementById("voice-select").value;
+
+    if (text !== "") {
+      let utterance = new SpeechSynthesisUtterance(text);
+      utterance.voice = voices[selectedVoice];
+      synth.speak(utterance);
+      
+      let faceImage = document.querySelector("img");
+      faceImage.setAttribute("src", "assets/images/smiling-open.png");
+
+      utterance.onend = function() {
+        faceImage.setAttribute("src", "assets/images/smiling.png");
+      }
+    }
+  });
 }
